5543c8b5e2dc0da453e938e6d100cb221e5106fa
Joel Machango <joelmachango@gmail.com>
2018-09-19 12:43:51 +0300

    Connect app to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connnected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
 
